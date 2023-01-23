dc8cf6649da8e5cb34a84a2af33ba1b57172d4ac
Artem Voytenko <bobinrobin12@gmail.com>
2023-01-24 00:16:40 +0300

    Вроде выполнил задание, создал метод который при помощи цикла записывает в себя числа

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using static System.Console;
+
+Clear();
+
+WriteLine("По условию задачи введите восемь чисел!");
+WriteLine($"[{String.Join(",", GreatArray(8))}]");
+
+
+int[] GreatArray(int size)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        Write("Введите число: ");
+        result[i] = int.Parse(ReadLine());
+    }
+    return result;
+}
