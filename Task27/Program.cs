8bf73a12d70ab0634bb7353bee128ee8c84555e0
Artem Voytenko <bobinrobin12@gmail.com>
2023-01-23 23:42:37 +0300

    выполнил второе задание, создал метод

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using static System.Console;
+
+Clear();
+
+WriteLine("Введите число: ");
+WriteLine($"Сумма цифр Вашего числа равна {GetSumInt(int.Parse(ReadLine()))}");
+
+
+int GetSumInt(int Number)
+{
+    int result = 0;
+    int tempNum = Number;
+    while (tempNum > 0)
+    {
+        result += tempNum % 10;
+        tempNum /= 10;
+    }
+    return result;
+}
