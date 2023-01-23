ca660c5b0ec917cf86e7c5f06d0ede9f75e539f9
Artem Voytenko <bobinrobin12@gmail.com>
2023-01-23 23:29:52 +0300

    выполнил задание, создал метод с циклом внутри

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using static System.Console;
+
+Clear();
+
+WriteLine("Введите число: ");
+int numA = int.Parse(ReadLine());
+WriteLine("В какую степень его возвести? ");
+int numB = int.Parse(ReadLine());
+
+WriteLine($"Ответ {GetExponent(numA, numB)}");
+
+
+
+
+int GetExponent(int number, int exponent)
+{
+    int result = number;
+    for (int i = 1; i < exponent; i++)
+    {
+        result *= number;
+    }
+    return result;
+}
